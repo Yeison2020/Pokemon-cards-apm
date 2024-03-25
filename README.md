25f639b0a6b81d1c50c65a43670f55caf3a6d73b
Yeison Casado <72528124+Yeison2020@users.noreply.github.com>
2024-03-25 08:20:18 -0400

    Readme

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
-[circleci-image]: https://img.shields.io/circleci/build/github/nestjs/nest/master?token=abc123def456
-[circleci-url]: https://circleci.com/gh/nestjs/nest
-
 ## Description
 
 [Nest](https://github.com/nestjs/nest) framework TypeScript starter repository.
 
-## Installation
-
-```bash
-$ npm install
-```
+<!-- ## Installation -->
 
 ## Running the app
 
 ```bash
+# Set execute permission
+
+chmod +x run.sh
+
+sh run.sh
+
 # Start
-$ npm run start
+
+ npm run start
 
 ## Stack
 
--- run.sh
+++ run.sh
@@ -1,3 +1,27 @@
-docker-compose down
+# Instance setup requirements 
+
+sudo yum update -y
+sudo yum install -y docker
+sudo yum install -y git
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+# Optional - need confirmation
+
+sudo chmod +x /usr/local/bin/docker-compose docker-compose version
+
+# installing NodeJS and NPM
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+nvm install --lts
+
+
+# Docker setup 
+# sudo usermod -a -G docker dd-agent
+
 docker-compose build
 docker-compose up -d
